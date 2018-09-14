8e35686ae34806f116fd51c6b37191c0ee4f6060
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-09-14 11:52:29 +0200

    Make it work

--- predic.rb
+++ predic.rb
@@ -1,22 +1,33 @@
-# create_order(mar)
-#
-def generate_orders(settings)
+def generate_orders(market_id, orders_settings)
   orders = []
-  settings[:amount].times do
+  market  = Market.find_by_id(market_id)
+  traders = Member.where(email: orders_settings[:traders].split(',')).pluck(:id)
+  orders_settings[:amount].times do
     orders << {
-      volume:     settings[:min_volume],
-      price:      settings[:min_price],
-      created_at: settings[:min_created_at],
-      updated_at: settings[:min_created_at]
+      volume:     orders_settings[:min_volume],
+      price:      orders_settings[:min_price],
+      created_at: orders_settings[:min_created_at],
+      updated_at: orders_settings[:min_created_at],
+      ord_type:   :limit,
+      bid:        market.quote_unit,
+      ask:        market.base_unit,
+      type:       orders_settings[:type] == 'bid' ? 'OrderBid' : 'OrderAsk',
+      member_id:  traders.sample,
+      market_id:  market.id,
+      state:      ::Order::WAIT
     }
   end
-end
-
-def save_orders
-
-end
-
-YAML.load_file('predictive_trading.yml' || []).deep_symbolize_keys.each do |market, orders|
   orders
-  binding.pry
+end
+
+YAML.load_file('predictive_trading.yml' || []).deep_symbolize_keys.each do |market, settings|
+  settings.map do |orders_settings|
+    generate_orders(market, orders_settings)
+      .yield_self do |orders_array|
+        orders_array.map {|order_hash| Order.new(order_hash) }
+      end
+      .yield_self do |orders_objects|
+        Ordering.new(orders_objects).submit
+      end
+  end
 end
